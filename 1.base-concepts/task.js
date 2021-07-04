08c47b0d2eb4aebaf95e0d8d19b3bdd0d436a742
Sergey Zlenskiy <zlenskiy@gmail.com>
2021-07-04 15:24:40 +0300

    add corrections 1

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -4,11 +4,9 @@ function solveEquation(a, b, c) {
   let arr = [];
   // код для задачи №1 писать здесь
   let D = b ** 2 - 4 * a * c;
-  if (D < 0) {
-    arr = [];
-  } else if (D === 0) {
-    arr = -b/(2*a);
-  } else {
+  if (D === 0) {
+    arr[0] = -b/(2*a);
+  } else if (D > 0) {
     arr[0] = (-b + Math.sqrt(D))/(2*a);
     arr[1] = (-b - Math.sqrt(D))/(2*a);
   }
@@ -19,17 +17,33 @@ function calculateTotalMortgage(percent, contribution, amount, date) {
   let totalAmount;
 
   // код для задачи №2 писать здесь
-  // let period;
-  // let realTime = new Date().toLocaleDateString();
-  // realTime = +realTime;
-  // let monthToPay = date.getMonth();
-  // let yearToPay = date.getYear();
-  // console.log(monthToPay, yearToPay, period, realTime);
-  // let percentRate = percent / 100; 
-  // let monthPercentRate = percentRate / 12;
-  // let loanBody = amount - contribution;
-  // let amountPerMonth = loanBody * (monthPercentRate + (monthPercentRate / (((1 + monthPercentRate) ** period) - 1)));
-  // totalAmount = amountPerMonth * date;
-  // console.log(percentRate, monthPercentRate,date,  loanBody, amountPerMonth,totalAmount, date);
+  let indicator,  //Переменная, указывающая на то, какой параметр введен неверно
+      parameterValue;
+  if (typeof percent === 'string') {
+    percent = +percent;
+    parameterValue = percent;
+    indicator = 'Процентная ставка';
+  } else if (typeof contribution === "string") {
+    contribution = +contribution;
+    parameterValue = contribution;
+    indicator = 'Начальный взнос';
+  } else if (typeof amount === 'string'){
+    amount = +amount;
+    parameterValue = amount;
+    indicator = 'Общая стоимость';
+  }
+  if ((typeof arguments !== 'number') && (typeof arguments !== 'string')) {
+    console.log(`Параметр ${indicator} содержит неправильное значение ${parameterValue}`);
+  }
+  let currentDate = new Date();
+  let countOfMonth = ((date.getMonth() - currentDate.getMonth())+(12*(date.getFullYear()-currentDate.getFullYear())));
+  //console.log(currentDate, date, countOfMonth);
+  let percentRate = percent / 100; 
+  let monthPercentRate = percentRate / 12;
+  let loanBody = amount - contribution;
+  let amountPerMonth = loanBody * (monthPercentRate + (monthPercentRate / (((1 + monthPercentRate) ** countOfMonth) - 1)));
+  totalAmount = amountPerMonth * countOfMonth;
+  totalAmount = Math.round(totalAmount * 100) / 100;
+  console.log(totalAmount);
   return totalAmount;
 }
