678aed3d65bfe564cdb7878709866037c09d34cc
Sergey Zlenskiy <zlenskiy@gmail.com>
2021-07-08 22:31:44 +0300

    add homework

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,14 +1,21 @@
 function compareArrays(arr1, arr2) {
   let result;
-
+  
   // Ваш код
-
+  if (arr1.length === arr2.length && arr1.every((item, idx) => item === arr2[idx])) {
+    result = true;
+    return result;
+  } else {
+    result = false;
+    return result;
+  }
   return result; // boolean
 }
 
 function advancedFilter(arr) {
   let resultArr;
-
+  resultArr = arr.filter((item) => item > 0);
+  resultArr = resultArr.filter((item) => item % 3 === 0).map((item, idx) => item * 10);
   // Ваш код
 
   return resultArr; // array
