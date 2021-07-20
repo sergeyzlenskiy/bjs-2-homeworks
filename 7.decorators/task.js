d57e96da930ef3cb0e20d6c27ea5a913aac6fdff
Sergey Zlenskiy <zlenskiy@gmail.com>
2021-07-20 21:02:06 +0300

    add some corrections

--- 7.decorators/task.js
+++ 7.decorators/task.js
@@ -1,57 +1,94 @@
  function cachingDecoratorNew(func) {
-  // Ваш код
-  let cache = [];
-  return function wrapper(...args) {
-    let hash = 0;
-    //console.log(args);
-    for (let item of args) {
-      hash += item;
-    }
-    let idx = cache.findIndex((item) => item === hash);
-    //console.log(hash, idx);
-    if (idx !== -1) {
-      console.log('Из кэша: ' + cache[idx]);
-      //console.log(cache);
-      return "Из кэша: " + cache[idx];
-    } else {
-      let result = func.call(this, ...args);
-      cache.push(result);
-      if (cache.length > 5) {
-        cache.shift();
-      }
-      console.log(cache);
-      console.log('Вычисляем: ' + result);
-      return "Вычисляем: " + result;
-    }
-  };
-}
+   // Ваш код
+   // let cache = [];
+   // let cache1 = [];
+   // return function wrapper(...args) {
+   //   let hash = '';
+   //   let joinedArgs = args.join('');
+   //   //console.log(joinedArgs);
+   //   for (let item of args) {
+   //     hash += item;
+   //   }
+   //   let idx = cache1.findIndex((item) => item.join('') === joinedArgs);
+   //   //console.log(cache, args, idx);
+   //   if (idx !== -1) {
+   //     console.log('Из кэша: ' + cache[idx]);
+   //     //console.log(cache);
+   //     return "Из кэша: " + cache[idx];
+   //   } else {
+   //     let result = func.call(this, ...args);
+   //     cache1.push(args);
+   //     cache.push(result);
+   //     if (cache.length > 5) {
+   //       cache.shift();
+   //       cache1.shift();
+   //     }
+   //     console.log(cache);
+   //     console.log('Вычисляем: ' + result);
+   //     return "Вычисляем: " + result;
+   //   }
+   // };
+ }
 
 
-function debounceDecoratorNew(func, ms) {
-  // Ваш код
-  let timeout;
-  
-  return function (...args) {
-    clearTimeout(timeout);
-    func.apply(this, args);
-    timeout = setTimeout(() => {
+ // function debounceDecoratorNew(func, ms) {
+ //   // Ваш код
+ //   let timeout;
 
-    }, ms);
-  
-  };
-}
+ //   return function (...args) {
 
-const sendSignal = () => console.log("Сигнал отправлен");
-const upgradedSendSignal = debounceDecoratorNew(sendSignal, 2000);
-setTimeout(upgradedSendSignal); // Сигнал отправлен
-setTimeout(upgradedSendSignal, 300); // проигнорировано так как от последнего вызова прошло менее 2000мс
-setTimeout(upgradedSendSignal, 900); // проигнорировано аналогично
-setTimeout(upgradedSendSignal, 1200); // проигнорировано аналогично
-setTimeout(upgradedSendSignal, 2300); // проигнорировано аналогично
-setTimeout(upgradedSendSignal, 4400); // Сигнал отправлен
-setTimeout(upgradedSendSignal, 4500); // проигнорировано аналогично
+ //     timeout = setTimeout(() => {
+ //       console.time("time");
+ //       func.apply(this, args);
+ //       clearTimeout(timeout);
+ //       console.timeEnd("time");
+ //     }, ms);
+
+ //   };
+ // }
 
 
-function debounceDecorator2(func) {
-  // Ваш код
-}
+ // ZADACHA 2
+
+
+
+ function debounceDecoratorNew(func, ms) {
+   let timeout;
+   let immediate = true;
+   return function (...args) {
+     clearTimeout(timeout);
+     if (immediate) {
+       func.apply(this, args);
+     }
+     immediate = false;
+     timeout = setTimeout(() => {
+       func.apply(this, args);
+     }, ms);
+   };
+ }
+
+ //ZADACHA 3
+
+
+ function debounceDecorator2(func) {
+   // Ваш код
+   function wrapper(...args) {
+     wrapper.count++;
+     return func.apply(this, args);
+  }
+ wrapper.count = 0;
+
+ return wrapper;
+ }
+
+
+ const sendSignal = () => console.log("Сигнал отправлен");
+ const upgradedSendSignal = debounceDecoratorNew(sendSignal, 2000);
+ const x = debounceDecorator2(debounceDecoratorNew);
+ setTimeout(upgradedSendSignal); // Сигнал отправлен
+ setTimeout(upgradedSendSignal, 300);
+ setTimeout(upgradedSendSignal, 900);
+ setTimeout(upgradedSendSignal, 2200); // Сигнал отправлен
+ setTimeout(upgradedSendSignal, 5300); // Сигнал отправлен
+ setTimeout(upgradedSendSignal, 7400);
+ console.log(x.count);
